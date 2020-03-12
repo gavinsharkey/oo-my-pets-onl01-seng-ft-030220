36617a1ee00a34a529e03d404a11dc3f71edde48
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-12 19:28:25 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,9 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  def initialize(name, owner)
+    @name = name
+    self.owner = owner
+    self.mood = 'nervous'
 end
