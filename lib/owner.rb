061a319fa401136ca64be01ac6841aa99ddb073d
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-12 19:32:58 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,12 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+
+  def initialize(name)
+    @name = name
+    @species = 'human'
+  end
+
+  def say_species
+    "I am a #{self.species}."
+  end
 end
