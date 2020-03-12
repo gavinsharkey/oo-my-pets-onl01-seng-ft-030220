662e4056e92f93aab31bf5befda3f413678bb319
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-12 20:01:30 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,13 +59,13 @@ class Owner
       cat.mood = 'nervous'
       cat.owner = nil
     end
-    
+
     self.dogs.each do |dog|
       dog.mood = 'nervous'
       dog.owner = nil
     end
   end
 
-  
+
 
 end
