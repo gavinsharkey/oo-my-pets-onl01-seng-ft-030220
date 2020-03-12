843f26ee2e581343e49ce17c52f98e28e709abe7
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-12 19:36:05 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,29 @@
 class Owner
   attr_reader :name, :species
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+
   def initialize(name)
     @name = name
     @species = 'human'
+
+    self.save
   end
 
   def say_species
